93ff1c04417e3665a89329e9f2c7f577181b3c0f
toruyoneji <toruyoneji0514@icloud.com>
2025-05-04 12:42:31 +0900

    💄 ✨ 各テストページのリンク、リンクページ設定

--- thankjapan/thank_japan_app/urls.py
+++ thankjapan/thank_japan_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import IndexView
+from .views import TopView, FoodView, NatureView, FashionView, CultureView, CookView, AppliancesView, AnimalView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("", TopView.as_view(), name="toppage"),
+    path("food/", FoodView.as_view(), name="foodpage"),
+    path("nature/", NatureView.as_view(), name="naturepage"),
+    path("fashion/", FashionView.as_view(), name="fashionpage"),
+    path("culture/", CultureView.as_view(), name="culturepage"),
+    path("cook/", CookView.as_view(), name="cookpage"),
+    path("appliances/", AppliancesView.as_view(), name="appliancespage"),
+    path("animal/", AnimalView.as_view(), name="animalpage"),
+
 ]
--- thankjapan/thank_japan_app/views.py
+++ thankjapan/thank_japan_app/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from .models import ThankJapanModel
 
-class IndexView(View):
-    def get(self,request):
-        return render(request, "index.html")
+class TopView(ListView):
+    template_name = "thank_japan_app/toppage.html"
+    model = ThankJapanModel
 
+class FoodView(ListView):
+    template_name = "thank_japan_app/food.html"
+    model = ThankJapanModel
+
+class NatureView(ListView):
+    template_name = "thank_japan_app/Nature.html"
+    model = ThankJapanModel
+
+class FashionView(ListView):
+    template_name = "thank_japan_app/fashion.html"
+    model = ThankJapanModel    
+
+class CultureView(ListView):
+    template_name = "thank_japan_app/culture.html"
+    model = ThankJapanModel
+
+class CookView(ListView):
+    template_name = "thank_japan_app/cook.html"
+    model = ThankJapanModel
+
+class AppliancesView(ListView):
+    template_name = "thank_japan_app/appliances.html"
+    model = ThankJapanModel
+
+class AnimalView(ListView):
+    template_name = "thank_japan_app/animal.html"
+    model = ThankJapanModel
