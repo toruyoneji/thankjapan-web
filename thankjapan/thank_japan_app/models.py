de48e33e9b0c1060a16e5d9ec7bc2b7f9e104b07
toruyoneji <toruyoneji0514@icloud.com>
2025-05-05 13:13:45 +0900

    🎨 カラムのdiscriptionをdescriptionに変更

--- thankjapan/thank_japan_app/models.py
+++ thankjapan/thank_japan_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class ThankJapanModel(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    discription = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000)
     image = models.ImageField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
