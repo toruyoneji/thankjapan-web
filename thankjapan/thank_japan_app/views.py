a946243ae7b6eac9998958b4e386244ee294f39f
toruyoneji <toruyoneji0514@icloud.com>
2025-05-10 03:30:47 +0900

    ♻️ get_queryset関数に変更

--- thankjapan/thank_japan_app/views.py
+++ thankjapan/thank_japan_app/views.py
@@ -16,48 +16,72 @@ class ImgDetailView(DetailView):
 
 class FoodView(ListView):
     template_name = "thank_japan_app/food.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="food")
 
 class NatureView(ListView):
     template_name = "thank_japan_app/Nature.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="nature")
 
 class FashionView(ListView):
     template_name = "thank_japan_app/fashion.html"
-    model = ThankJapanModel    
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="fashion")
 
 class CultureView(ListView):
     template_name = "thank_japan_app/culture.html"
-    model = ThankJapanModel
-
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="culture")
+    
 class CookView(ListView):
     template_name = "thank_japan_app/cook.html"
-    model = ThankJapanModel
-
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="cook")
+    
 class AppliancesView(ListView):
     template_name = "thank_japan_app/appliances.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="appliances")
 
 class AnimalView(ListView):
     template_name = "thank_japan_app/animal.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="animal")
 
 class BuildingView(ListView):
     template_name = "thank_japan_app/building.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="building")
 
 class FlowerView(ListView):
     template_name = "thank_japan_app/flower.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="flower")
 
 class HouseholdItemsView(ListView):
     template_name = "thank_japan_app/householditems.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="householditems")
 
 class SportsView(ListView):
     template_name = "thank_japan_app/sports.html"
-    model = ThankJapanModel
-
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="sports")
+    
 class WorkView(ListView):
     template_name = "thank_japan_app/work.html"
-    model = ThankJapanModel
+    
+    def get_queryset(self):
+        return ThankJapanModel.objects.filter(category="work")
