0a0b80a6be2d9ebf2405702c638407fdefe84ae5
toruyoneji <toruyoneji0514@icloud.com>
2025-05-17 07:12:46 +0900

    ⚡️ item25->13change

--- thankjapan/thankjapan/settings.py
+++ thankjapan/thankjapan/settings.py
@@ -1,16 +1,18 @@
-
-
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gn(g9g$nau2k3c5tuz88ob+as3mz1z5_7ssso1zd^b^@j!f@re'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,16 +73,27 @@ WSGI_APPLICATION = 'thankjapan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'thankjapanDB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'thankjapanDB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': os.environ.get('NAME'),
+         'USER': os.environ.get('USER'),
+         'PASSWORD': os.environ.get('PASSWORD'),
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
 
 
 # Password validation
