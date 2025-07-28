13a362544d872e3df9703e92629e47e25fbfdbbf
toruyoneji <toruyoneji0514@icloud.com>
2025-07-28 12:37:49 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Thank_Japan_django" 
+"# thank-japan-django-2" 
---
+++ thank_japan_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class ThankJapanModel(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    discription = models.TextField(max_length=1000)
+    image = models.ImageField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + "(" + self.category + ")"
