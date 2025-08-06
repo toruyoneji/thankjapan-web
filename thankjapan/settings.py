20b3129107fd0ff72a785b831ce589a4687f35d5
toruyoneji <toruyoneji0514@icloud.com>
2025-08-06 20:54:53 +0900

    localhost add

--- thankjapan/settings.py
+++ thankjapan/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['https://thankjapan-aaa090ee21df.herokuapp.com/']
+ALLOWED_HOSTS = ['https://thankjapan-aaa090ee21df.herokuapp.com/', 'localhost']
 
 
 # Application definition
