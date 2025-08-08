f341d4bb49ccf402cfe879a384bc7e00451cc120
toruyoneji <toruyoneji0514@icloud.com>
2025-08-08 14:13:08 +0900

    model change

--- thank_japan_app/models.py
+++ thank_japan_app/models.py
@@ -12,6 +12,28 @@ class ThankJapanModel(models.Model):
 
     def __str__(self):
         return self.name + "(" + self.category + ")"
+    
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+class ThankJapanImageModel(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    image = CloudinaryField('image', folder='thankjapan/images')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+
+class ThankJapanBackgroundModel(models.Model):
+    background_image = CloudinaryField('background_image', folder='thankjapan/backgrounds/', blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.background_image
+
 
 
 
