d591b6cdbae45d0ded1fa2b2585b294a6f757481
toruyoneji <toruyoneji0514@icloud.com>
2025-08-08 14:33:36 +0900

    admin add

--- thank_japan_app/admin.py
+++ thank_japan_app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import ThankJapanModel
+from .models import ThankJapanModel, ThankJapanBackgroundModel
 
 class ThankJapanAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     
+class ThankJapanBackgroundAdmin(admin.ModelAdmin):
+    list_display = ('background_image')
     
 admin.site.register(ThankJapanModel, ThankJapanAdmin)
+admin.site.register(ThankJapanBackgroundModel, ThankJapanBackgroundAdmin)
 
