c0454db5f7a1f8e38d37157b6fef8d1fc1f5ff51
toruyoneji <toruyoneji0514@icloud.com>
2025-08-10 21:18:41 +0900

    robots2

--- thankjapan/urls.py
+++ thankjapan/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from thank_japan_app.views import robots_txt
 
 app_name = "thank_japan_app"
 urlpatterns = [
+    path('robots.txt', robots_txt),
     path('kanrisha/', admin.site.urls),
     path('', include('thank_japan_app.urls')),
    
