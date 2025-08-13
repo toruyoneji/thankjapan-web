4be96a85cb455a913d54c8e962df98a0a839b61e
toruyoneji <toruyoneji0514@icloud.com>
2025-08-13 22:50:48 +0900

    pay

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from  payment.views import create_checkout_session, success, cancel, checkout
+from  payment.views import create_checkout_session, success, cancel, checkout, stripe_webhook
 
 urlpatterns = [
     path('create-checkout-session/', create_checkout_session, name='create_checkout_session'),
     path('success/', success, name='success'),
     path('cancel/', cancel, name='cancel'),
     path('checkout/', checkout, name='checkout'),
+    path('webhook/stripe/', stripe_webhook, name='stripe-webhook'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -8,7 +8,7 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 @csrf_exempt
 def create_checkout_session(request):
-    YOUR_DOMAIN = "http://www.thankjapan.com" 
+    YOUR_DOMAIN = "https://www.thankjapan.com" 
 
     checkout_session = stripe.checkout.Session.create(
         payment_method_types=['card'],
@@ -19,7 +19,7 @@ def create_checkout_session(request):
                     'product_data': {
                         'name': 'ThankJapan support',  
                     },
-                    'unit_amount': 2000,
+                    'unit_amount': 20000,
                 },
                 'quantity': 1,
             },
@@ -44,3 +44,34 @@ def success(request):
 
 def cancel(request):
     return render(request, 'cancel.html')
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
+
+    
+    event = None
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        return JsonResponse({'error': 'Invalid payload'}, status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return JsonResponse({'error': 'Invalid signature'}, status=400)
+
+    
+    if event['type'] == 'payment_intent.succeeded':
+        payment_intent = event['data']['object']
+        print('PaymentIntent was successful!', payment_intent)
+    
+
+    elif event['type'] == 'payment_intent.failed':
+        payment_intent = event['data']['object']
+        print('PaymentIntent failed.', payment_intent)
+
+    return JsonResponse({'status': 'success'}, status=200)
+
