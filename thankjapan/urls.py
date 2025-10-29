8ef1c7bc5b6db0fc718688e4cb2bb5e89363be81
toruyoneji <toruyoneji0514@icloud.com>
2025-10-29 21:44:20 +0900

    change set

--- thankjapan/urls.py
+++ thankjapan/urls.py
@@ -5,8 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from thank_japan_app.views import robots_txt
 from django.views.generic import TemplateView
-from django.conf.urls.i18n import i18n_patterns
-from payment.views import payment_success, cancel_view
 
 app_name = "thank_japan_app"
 urlpatterns = [
@@ -15,10 +13,10 @@ urlpatterns = [
     path('support/', include('payment.urls')),
     path('', include('thank_japan_app.urls')),
    
+    
     path('sitemap.xml', TemplateView.as_view(
         template_name='sitemap.xml', content_type='application/xml')),
    
 ]
-
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
