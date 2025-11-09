e0a8cddcb0d7e4ac49fb0b4a2e8148a25ae5f6f2
toruyoneji <toruyoneji0514@icloud.com>
2025-11-09 10:03:34 +0900

    test top

--- thankjapan/settings.py
+++ thankjapan/settings.py
@@ -11,7 +11,7 @@ GA_TRACKING_ID = os.environ.get('GA_TRACKING_ID', '')
 
 # Load environment variables
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     'www.thankjapan.com',
     'thankjapan.com',
