1eb4b93fef808cd000a5f392473658f4f28e8f6d
toruyoneji <toruyoneji0514@icloud.com>
2026-02-22 11:05:52 +0900

    tagengo

--- thank_japan_app/models.py
+++ thank_japan_app/models.py
@@ -13,11 +13,48 @@ class ThankJapanModel(models.Model):
     kanji_name = models.CharField(max_length=100, blank=True, null=True) 
     katakana_name = models.CharField(max_length=100, blank=True, null=True)
     category = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
-    history = models.TextField(max_length=1000)
     image = CloudinaryField('image', folder='thankjapan/images')
     timestamp = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, null=False, blank=False)
+    description = models.TextField(max_length=1000)
+    history = models.TextField(max_length=1000)
+
+    description_en_in = models.TextField(max_length=1000, blank=True, null=True)
+    history_en_in = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_ja = models.TextField(max_length=1000, blank=True, null=True)
+    history_ja = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_zh_cn = models.TextField(max_length=1000, blank=True, null=True)
+    history_zh_cn = models.TextField(max_length=1000, blank=True, null=True)
+    description_zh_hant = models.TextField(max_length=1000, blank=True, null=True)
+    history_zh_hant = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_ko = models.TextField(max_length=1000, blank=True, null=True)
+    history_ko = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_fr = models.TextField(max_length=1000, blank=True, null=True)
+    history_fr = models.TextField(max_length=1000, blank=True, null=True)
+    description_de = models.TextField(max_length=1000, blank=True, null=True)
+    history_de = models.TextField(max_length=1000, blank=True, null=True)
+    description_it = models.TextField(max_length=1000, blank=True, null=True)
+    history_it = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_es_es = models.TextField(max_length=1000, blank=True, null=True)
+    history_es_es = models.TextField(max_length=1000, blank=True, null=True)
+    description_es_mx = models.TextField(max_length=1000, blank=True, null=True)
+    history_es_mx = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_pt = models.TextField(max_length=1000, blank=True, null=True)
+    history_pt = models.TextField(max_length=1000, blank=True, null=True)
+    description_pt_br = models.TextField(max_length=1000, blank=True, null=True)
+    history_pt_br = models.TextField(max_length=1000, blank=True, null=True)
+
+    description_th = models.TextField(max_length=1000, blank=True, null=True)
+    history_th = models.TextField(max_length=1000, blank=True, null=True)
+    description_vi = models.TextField(max_length=1000, blank=True, null=True)
+    history_vi = models.TextField(max_length=1000, blank=True, null=True)
+
 
     def save(self, *args, **kwargs):
         if not self.slug:
