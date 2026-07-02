083d9749e41c27c7cdf9b93aa14df7d3cea4f35b
toruyoneji <toruyoneji0514@icloud.com>
2026-07-03 06:06:01 +0900

    key

--- thankjapan/urls.py
+++ thankjapan/urls.py
@@ -23,10 +23,7 @@ def assetlinks_view(request):
                 "namespace": "android_app",
                 "package_name": "com.thankjapan.www.twa",
                 "sha256_cert_fingerprints": [
-                    "1E:CA:F1:51:07:5D:AE:E3:8E:AB:D3:04:A9:4A:E8:9B:E3:6A:80:6D:9B:80:3E:F2:D6:10:56:76:42:D0:E4:80",
-		                "6C:FB:68:16:47:71:D0:00:D0:DE:D3:B4:C6:1A:78:F1:03:E5:7F:0B:7B:8E:A1:5B:2E:99:CB:1C:2F:D0:B6:90",
-        	          "6C:FB:68:16:47:71:D0:00:D0:DE:D3:B4:C6:1A:7B:F1:03:E5:7F:0B:7B:8E:A1:5B:2E:99:CB:1C:2F:D0:B6:90",
-        	          "6C:FB:68:16:47:71:D0:00:D0:DE:D3:B4:C6:1A:78:F1:03:E5:7F:0B:7B:8E:A1:5B:2E:99:CB:1C:2F:D0:B6:90"
+                    "6C:FB:68:16:47:71:D0:00:D0:DE:D3:B4:C6:1A:78:F1:03:E5:7F:0B:7B:8E:A1:5B:2E:99:CB:1C:2F:D0:B6:90"
                 ]
             }
         }
